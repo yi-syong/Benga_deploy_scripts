8ff7bb3fd11e967a8aed5164eb4d3ca84a56cda2
yi-syong <35520228+yi-syong@users.noreply.github.com>
2020-02-26 10:06:29 +0800

    Update blast

--- install-blast.sh
+++ install-blast.sh
@@ -1,3 +1,3 @@
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.9.0+-x64-linux.tar.gz
-tar zxvf ncbi-blast-2.9.0+-x64-linux.tar.gz
-sudo cp ncbi-blast-2.9.0+/bin/* /usr/bin/
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.10.0/ncbi-blast-2.10.0+-x64-linux.tar.gz
+tar zxvf ncbi-blast-2.10.0+-x64-linux.tar.gz
+sudo cp ncbi-blast-2.10.0+/bin/* /usr/bin/
